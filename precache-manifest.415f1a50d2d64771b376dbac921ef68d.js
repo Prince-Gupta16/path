16055cc928a14b2cff81c35b460ce16fbf5f0284
Prince Gupta <princgupta27900@gmail.com>
2021-12-11 00:51:43 +0530

    Updates

---
+++ precache-manifest.415f1a50d2d64771b376dbac921ef68d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c077e240657aa14981815b2736784a",
+    "url": "/path/index.html"
+  },
+  {
+    "revision": "cb2790c7312eec1840f1",
+    "url": "/path/static/css/2.7118e3f5.chunk.css"
+  },
+  {
+    "revision": "f2268623e87f5df482aa",
+    "url": "/path/static/css/main.f9d29a4b.chunk.css"
+  },
+  {
+    "revision": "cb2790c7312eec1840f1",
+    "url": "/path/static/js/2.ebb8bed6.chunk.js"
+  },
+  {
+    "revision": "7140738e8b1357f8b65a93a934f3b728",
+    "url": "/path/static/js/2.ebb8bed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2268623e87f5df482aa",
+    "url": "/path/static/js/main.ccf3bb69.chunk.js"
+  },
+  {
+    "revision": "3df72acba607428a0366",
+    "url": "/path/static/js/runtime-main.342ece5e.js"
+  }
+]);
